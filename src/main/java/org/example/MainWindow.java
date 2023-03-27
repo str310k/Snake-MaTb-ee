a5e561b7053bcce6569129dc9249bba62567efdc
str310k <you@example.com>
2023-03-27 19:26:32 +0300

    new commit

---
+++ src/main/java/org/example/MainWindow.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.*;
+
+
+public class MainWindow extends JFrame {
+
+    public MainWindow() {
+
+        setTitle("змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(565, 540);
+        setLocation(500, 250);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainWindow MW = new MainWindow();
+    }
+}
